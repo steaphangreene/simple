574d0759c0bdd6bd1866179ed3d72393cb349201
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2005-03-04 07:26:30 +0000

    Added script-generated stubs for each planned compound widget. Updated TODO list. Updated Makefile.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@121 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

---
+++ simplegui/sg_multitab.h
@@ -0,0 +1,45 @@
+// *************************************************************************
+//  This file is part of the SimpleGUI Example Module by Steaphan Greene
+//
+//  Copyright 2005 Steaphan Greene <stea@cs.binghamton.edu>
+//
+//  SimpleGUI is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  SimpleGUI is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with SimpleGUI (see the file named "COPYING");
+//  if not, write to the the Free Software Foundation, Inc.,
+//  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// *************************************************************************
+
+// This file was created from (or actually IS) a basic compound widget
+// def, so it's not defined and is really just a place-holder for now.
+
+#ifndef SG_MULTITAB_H
+#define SG_MULTITAB_H
+
+#include "sg_compound.h"
+
+class SG_Button;
+class SG_TextArea;
+
+class SG_MultiTab : public SG_Compound {
+public:
+  SG_MultiTab();
+  virtual ~SG_MultiTab();
+//  virtual bool SetDefaultCursor(GL_MODEL *cur);
+  virtual int ChildEvent(SDL_Event *event);
+  
+protected:
+//  static GL_MODEL Default_Mouse_Cursor;
+  SG_Button *okb;
+  };
+
+#endif // SG_MULTITAB_H
---
+++ simplegui/sg_scrollingarea.cpp
@@ -0,0 +1,65 @@
+// *************************************************************************
+//  This file is part of the SimpleGUI Example Module by Steaphan Greene
+//
+//  Copyright 2005 Steaphan Greene <stea@cs.binghamton.edu>
+//
+//  SimpleGUI is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  SimpleGUI is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with SimpleGUI (see the file named "COPYING");
+//  if not, write to the the Free Software Foundation, Inc.,
+//  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// *************************************************************************
+
+// This file was created from (or actually IS) a basic compound widget
+// def, so it's not defined and is really just a place-holder for now.
+
+#include <GL/gl.h>
+
+#include "sg_scrollingarea.h"
+#include "sg_panel.h"
+#include "sg_button.h"
+#include "sg_events.h"
+
+SG_ScrollingArea::SG_ScrollingArea()
+	: SG_Compound(8, 5, 0.1, 0.1) {
+  background = new SG_Panel(SG_COL_FG);
+  okb = new SG_Button("Ok", SG_COL_RAISED, SG_COL_TEXT, SG_COL_LOW);
+  AddWidget(okb, 7, 4, 1, 1);
+  SG_Widget *labelb =
+	new SG_TextArea("SG_ScrollingArea", SG_COL_LOW, SG_COL_TEXT);
+  AddWidget(labelb, 1, 2, 6, 1);
+  }
+
+SG_ScrollingArea::~SG_ScrollingArea() {	//Even Need this?
+  vector<SG_Widget *> tmp = widgets;
+  vector<SG_Widget *>::iterator itr = tmp.begin();
+  widgets.clear();
+  wgeom.clear();
+  for(; itr != tmp.end(); ++itr) {
+    if(*itr) delete (*itr);
+    }
+  }
+
+int SG_ScrollingArea::ChildEvent(SDL_Event *event) {
+  if(event->user.code == SG_EVENT_BUTTONPRESS) {
+    if(event->user.data1 == (void *)(okb)) {
+      event->user.code = SG_EVENT_OK;
+      event->user.data1 = (void*)this;
+      return 1;
+      }
+    }
+  return 0; // Silence children doing other things
+  }
+
+//  bool SG_ScrollingArea::SetDefaultCursor(GL_MODEL *cur);
+  
+//  static GL_MODEL SG_ScrollingArea::Default_Mouse_Cursor = NULL;
