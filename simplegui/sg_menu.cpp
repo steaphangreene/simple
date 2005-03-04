a3f780ec106fa7c8307581dd1d6cb60457f69e27
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2005-03-04 20:09:24 +0000

    Added Menu widget stub.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@125 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

---
+++ simplegui/sg_menu.cpp
@@ -0,0 +1,43 @@
+// *************************************************************************
+//  This file is part of the SimpleGUI Example Module by Steaphan Greene
+//
+//  Copyright 2005 Steaphan Greene <stea@cs.binghamton.edu>
+//
+//  SimpleGUI is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  SimpleGUI is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with SimpleGUI (see the file named "COPYING");
+//  if not, write to the the Free Software Foundation, Inc.,
+//  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// *************************************************************************
+
+#include <GL/gl.h>
+
+#include "sg_menu.h"
+#include "sg_globals.h"
+#include "sg_events.h"
+
+SG_Menu::SG_Menu(string itms, int c, int tc, int ac)
+		: SG_TextArea(itms, c, tc) {
+  texture.resize(2);
+  texture[1] = texture[0];
+  texture[1].col = *(current_sg->Color(ac));
+  BuildTexture(texture[1], message, 0.0, 0.0);
+  }
+
+SG_Menu::~SG_Menu() {
+  }
+
+//  bool SG_Menu::SetDefaultCursor(GL_MODEL *cur);
+  
+//  static GL_MODEL SG_Menu::Default_Mouse_Cursor = NULL;
+
+
---
+++ simplegui/sg_menu.h
@@ -0,0 +1,39 @@
+// *************************************************************************
+//  This file is part of the SimpleGUI Example Module by Steaphan Greene
+//
+//  Copyright 2005 Steaphan Greene <stea@cs.binghamton.edu>
+//
+//  SimpleGUI is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  SimpleGUI is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with SimpleGUI (see the file named "COPYING");
+//  if not, write to the the Free Software Foundation, Inc.,
+//  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// *************************************************************************
+
+#ifndef SG_MENU_H
+#define SG_MENU_H
+
+#include "sg_textarea.h"
+
+class SG_Menu : public SG_TextArea {
+public:
+  SG_Menu(string itms, int c, int tc, int ac);
+		//The idea is that newlines will separate menuitems
+  virtual ~SG_Menu();
+//  virtual bool SetDefaultCursor(GL_MODEL *cur);
+  
+protected:
+//  static GL_MODEL Default_Mouse_Cursor;
+  };
+
+#endif // SG_MENU_H
+
