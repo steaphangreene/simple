df266e3b017ee0dc2410d76848b6d7b4d4a5f98a
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2005-03-09 15:45:15 +0000

    Removed some SG_Table duplicated code that was in SG_Compound.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@171 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

--- simplegui/sg_compound.cpp
+++ simplegui/sg_compound.cpp
@@ -28,47 +28,17 @@ SG_Compound::SG_Compound(int xsz, int ysz, float xbor, float ybor)
 	: SG_Table(xsz, ysz, xbor, ybor) {
   }
 
-SG_Compound::~SG_Compound() {	//Even need this?
-  vector<SG_Widget *> tmp = widgets;
-  vector<SG_Widget *>::iterator itr = tmp.begin();
-  widgets.clear();
-  wgeom.clear();
-  for(; itr != tmp.end(); ++itr) {
-    if(*itr) delete (*itr);
-    }
+SG_Compound::~SG_Compound() {
   }
 
 bool SG_Compound::HandleEvent(SDL_Event *event, float x, float y) {
 //  if(event->type == SDL_MOUSEBUTTONDOWN)
 //    fprintf(stderr, "Compound/Handle: Button Down at (%f,%f)\n", x, y);
 
-  int ret = 1;
-
-  vector<SG_Widget *>::iterator itrw = widgets.begin();
-  vector<SG_TableGeometry>::iterator itrg = wgeom.begin();
-  for(; itrw != widgets.end(); ++itrw, ++itrg) {
-    CalcGeometry(itrg);
-    if(x >= cur_geom.xp-cur_geom.xs && x <= cur_geom.xp+cur_geom.xs
-	&& y >= cur_geom.yp-cur_geom.ys && y <= cur_geom.yp+cur_geom.ys) {
-      x -= cur_geom.xp;	//Scale the coordinates to widget's relative coords
-      y -= cur_geom.yp;
-      x /= cur_geom.xs;
-      y /= cur_geom.ys;
-      ret = (*itrw)->HandleEvent(event, x, y);
-      break;
-      }
-    }
-
+  int ret = SG_Table::HandleEvent(event, x, y);
   if(ret && event->type == SDL_SG_EVENT) {
     return ChildEvent(event);
     }
-
-  if(background) ret = background->HandleEvent(event, x, y);
-
-  if(ret && event->type == SDL_SG_EVENT) {
-    return ChildEvent(event);
-    }
-
   return ret;
   }
 
@@ -79,33 +49,10 @@ bool SG_Compound::HandMouseEventTo(SG_Widget *targ, SDL_Event *event,
 
   if(targ == this) return HandleEvent(event, x, y);
 
-  int ret = 1;
-
-  vector<SG_Widget *>::iterator itrw = widgets.begin();
-  vector<SG_TableGeometry>::iterator itrg = wgeom.begin();
-  for(; itrw != widgets.end(); ++itrw, ++itrg) {
-    CalcGeometry(itrg);
-    if((*itrw)->HasWidget(targ)) {
-      x -= cur_geom.xp;	//Scale the coordinates to widget's relative coords
-      y -= cur_geom.yp;
-      x /= cur_geom.xs;
-      y /= cur_geom.ys;
-      ret = (*itrw)->HandMouseEventTo(targ, event, x, y);
-      break;
-      }
-    }
-
+  int ret = SG_Table::HandMouseEventTo(targ, event, x, y);
   if(ret && event->type == SDL_SG_EVENT) {
     return ChildEvent(event);
     }
-
-  if(background->HasWidget(targ))
-    ret = background->HandMouseEventTo(targ, event, x, y);
-
-  if(ret && event->type == SDL_SG_EVENT) {
-    return ChildEvent(event);
-    }
-
   return ret;
   }
 
