16c3f4a496a312607ae57577df5434b14fb5b8aa
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2006-04-01 16:53:41 +0000

    Updated copyright dates.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@597 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

--- simplevideo/sv.cpp
+++ simplevideo/sv.cpp
@@ -1,7 +1,7 @@
 // *************************************************************************
 //  This file is part of the SimpleVideo Example Module by Steaphan Greene
 //
-//  Copyright 2005 Steaphan Greene <stea@cs.binghamton.edu>
+//  Copyright 2005-2006 Steaphan Greene <stea@cs.binghamton.edu>
 //
 //  SimpleVideo is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
