12bbd8493f072c0be6ad0931aea1a315b44e39b0
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2006-04-06 10:04:22 +0000

    Added scripts directory and upload-changelog support scripts. Updated license headers.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@12 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

--- simpleconfig/simpleconfig.h
+++ simpleconfig/simpleconfig.h
@@ -1,20 +1,20 @@
 // *************************************************************************
-//  This file is part of the SimpleGUI Example Module by Steaphan Greene
+//  This file is part of the SimpleConfig Example Module by Steaphan Greene
 //
 //  Copyright 2005-2006 Steaphan Greene <stea@cs.binghamton.edu>
 //
-//  SimpleGUI is free software; you can redistribute it and/or modify
+//  SimpleConfig is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
 //  (at your option) any later version.
 //
-//  SimpleGUI is distributed in the hope that it will be useful,
+//  SimpleConfig is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with SimpleGUI (see the file named "COPYING");
+//  along with SimpleConfig (see the file named "COPYING");
 //  if not, write to the the Free Software Foundation, Inc.,
 //  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 // *************************************************************************
