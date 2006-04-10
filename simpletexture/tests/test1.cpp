689659223fb8845d4fb5f4024aa84b33caac18d4
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2006-04-10 02:37:03 +0000

    Added test1 which just shows an image specified on the command line.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@555 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

---
+++ simpletexture/tests/test1.cpp
@@ -0,0 +1,94 @@
+// *************************************************************************
+//  This file is part of the SimpleModel Example Module by Steaphan Greene
+//
+//  Copyright 2005-2006 Steaphan Greene <stea@cs.binghamton.edu>
+//
+//  SimpleModel is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  SimpleModel is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with SimpleModel (see the file named "COPYING");
+//  if not, write to the the Free Software Foundation, Inc.,
+//  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// *************************************************************************
+
+#include "SDL.h"
+#include "SDL_opengl.h"
+#include "SDL_keysym.h"
+
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+#include "../simpletexture.h"
+#include "renderer.h"
+
+int main(int argc, char **argv) {
+  int xs = 1024, ys = 768;
+
+  if(argc != 2) {
+    fprintf(stderr, "ERROR: requires file argument\n");
+    exit(1);
+    }
+
+  SimpleTexture *tex = new SimpleTexture(argv[1]);
+
+  if(!init_renderer(xs, ys)) {
+    fprintf(stderr, "Warning!  Graphics failed to init!\n");
+    }
+
+  int quit = 0;
+  while(!quit) {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+      if(event.type == SDL_QUIT) {
+	quit = 1;
+	}
+      else if(event.type == SDL_KEYDOWN) {
+        if(event.key.keysym.sym == SDLK_ESCAPE) quit = 1;
+        }
+      }
+    start_scene();
+
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+
+    glEnable(GL_BLEND);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, tex->GLTexture());
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, tex->yfact);
+    glVertex3f(-1.0, -1.0, 0.0);
+    glTexCoord2f(tex->xfact, tex->yfact);
+    glVertex3f( 1.0, -1.0, 0.0);
+    glTexCoord2f(tex->xfact, 0.0);
+    glVertex3f( 1.0, 1.0, 0.0);
+    glTexCoord2f(0.0, 0.0);
+    glVertex3f(-1.0, 1.0, 0.0);
+    glEnd();
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glDisable(GL_TEXTURE_2D);
+    glDisable(GL_BLEND);
+
+    finish_scene();
+    }
+
+  return 0;
+  }
