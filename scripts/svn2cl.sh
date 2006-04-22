c38c8cc0da96903dcca034f27c63308ab02d5d23
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2006-04-22 21:49:09 +0000

    Added missing license info to svn2cl.sh script.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@665 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

--- scripts/svn2cl.sh
+++ scripts/svn2cl.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
+# *************************************************************************
+#  This file is part of the Simple* Example Suite by Steaphan Greene
+#
+#  Copyright 2005-2006 Steaphan Greene <stea@cs.binghamton.edu>
+#
+#  Simple* is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  Simple* is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Simple* (see the file named "COPYING");
+#  if not, write to the the Free Software Foundation, Inc.,
+#  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# *************************************************************************
+
 svn log -r HEAD:1 | sed 's-(no author)-Unknown-g' | sed 's-^r[0-9]* | \([A-Za-z0-9]*\) | \([0-9-]* [0-9]*:[0-9]*\):.*$-|\2  \1|-g' | grep -v '^$' | grep -v -- ------------------------------------------------------------------------ | sed 's-^\([^|]\)-\t* \1-g' | tr '|' '\n'
