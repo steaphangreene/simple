97103d3bcc49b1288bd574343b6609f7bf3717c0
stea <stea@bb5c8b44-a00f-0410-a998-867a3eb9c5a3>
2008-05-10 13:52:54 +0000

    Integrated latest tester changes into SimpleNetwork's test1 and test2 programs.
    git-svn-id: https://www.cs.binghamton.edu/svn/simple@894 bb5c8b44-a00f-0410-a998-867a3eb9c5a3

--- simpleutils/tests/test1.cpp
+++ simpleutils/tests/test1.cpp
@@ -1,39 +1,59 @@
-#if 0
-#!/bin/sh
-g++ -Wall `sdl-config --cflags` sc_conn.cpp tests/tc.cpp -o client `sdl-config --libs` -lSDL_net
-
-exit
-#endif
-
 #include "../simplenetwork.h"
 
 using namespace std;
 
 int main()
 {
+	SDL_Init(0);
 	SimpleNetwork c = SimpleNetwork();
 	IPaddress ip;
 	char str[1024];
 	int msgs = 0;
 	SDLNet_ResolveHost(&ip, "127.0.0.1", 4052);
+	int curslot = -1;
 	int slotnum = c.Connect(ip, "Hello", "HI");
+	SDLNet_ResolveHost(&ip, "127.0.0.1", 4052);
+	int slotnum2 = c.Connect(ip, "Funk", "ADELiC");
 
-	fprintf(stderr, "connected? %d\n", c.IsConnected(slotnum));
-	fprintf(stderr, "slotnum: %d\n", slotnum);
-	c.Add(slotnum, "aword");
-	c.Add(slotnum, (Uint8)3);
+	fprintf(stderr, "connected? %d, %d\n", c.IsConnected(slotnum), c.IsConnected(slotnum2));
+	fprintf(stderr, "slotnum: %d, %d\n", slotnum, slotnum2);
+
+	SDL_Delay(30);
+	c.Add(slotnum,"Worm");
 	c.Send(slotnum);
 
-	while (c.IsConnected(slotnum) == SN_CONN_OK && msgs < 10)
+	SDL_Delay(3000);
+	c.Disconnect(slotnum);
+	c.Disconnect(slotnum2);
+
+	SDL_Delay(5000);
+	fprintf(stderr, "RESOLVE HOST\n");
+	SDLNet_ResolveHost(&ip, "bumblebee.cs.binghamton.edu", 4052);
+	slotnum = c.Connect(ip, "Hello", "HI");
+	fprintf(stderr, "RECON\n");
+
+	SDL_Delay(40);
+	c.Add(slotnum, "FRESH");
+	c.Send(slotnum);
+	SDL_Delay(4000);
+
+/*
+	while (c.IsConnected(slotnum) == SimpleNetwork::CONN_OK && msgs < 10)
 	{
-		scanf("%s", (char*)(str));
+		scanf("%s", &str);
+		if (msgs % 2 == 0)
+			curslot=slotnum;
+		else
+			curslot=slotnum2;
+
 		c.Add(slotnum, str);
 		c.Send(slotnum);
+
 		msgs++;
 	}
-//	c.Add(slotnum, (Uint16)23);
-//	c.Send(slotnum);
+*/
 	c.Disconnect(slotnum);
+	c.Disconnect(slotnum2);
 	fprintf(stderr, "connected? %d\n", c.IsConnected(slotnum));
 
 	return 0;
--- simpleutils/tests/test2.cpp
+++ simpleutils/tests/test2.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()
 {
+	SDL_Init(0);
 	int accepted= 0;
+	string tmp;
 	SimpleNetwork c = SimpleNetwork();
-	c.StartAccepting(5);
+	c.StartAccepting(10);
 
 	fprintf(stderr, "%d connections\n", accepted);
-	while (c.NumConnections() != 10)
+	while (c.NumConnections() < 2)
 	{
 		if (accepted != c.NumConnections())
 		{
@@ -18,6 +20,12 @@ int main()
 		}
 	}
 	fprintf(stderr, "%d connections\n", accepted);
+	c.StopAccepting();
+
+	SDL_Delay(300);
+	c.Recv(0,tmp);
+	fprintf(stderr,"tmp: %s\n", tmp.c_str());
+	while(true);
 
 	return 0;
 }
